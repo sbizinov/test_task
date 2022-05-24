828fb0b45202b78343e7c197a33331c24cedc91b
= <=>
2022-05-24 20:58:09 +0300

    task4

---
+++ task_4/www/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "mysql";
+$username = "root";
+$password = "password";
+$db = "onlineshop";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
